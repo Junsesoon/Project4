1b14ab0ae313f057448eafc0c0e776205bf05fe9
junsesoon <junsesoon@gmail.com>
2021-12-09 22:05:50 +0900

    1-2) 나이브 베이즈

--- Project4_C3.R
+++ Project4_C3.R
@@ -17,6 +17,30 @@ library(neuralnet)
 # 위스콘신 유방암 데이터셋을 대상으로 분류기법 2개를 적용하여 기법별 결과를 비교하시오.
 # 종속변수는 diagnosis: Benign(양성), Malignancy(악성)
 
+# 1-2) 나이브 베이즈 ####
+# (1)데이터 전처리
+data1 <- read.csv('C:/rwork/wdbc.csv',header = F)
+wisconsin2 <- data1 %>% select(2:12,) %>% 
+  `colnames<-`(c('양성여부','반경','질감','둘레','면적','매끄러움','조그만정도',
+                 '오목함','오목점','대칭','프랙탈차원'))
+
+# (2) 학습 데이터, 검정 데이터 생성
+# wisconsin$양성여부 <- as.character(wisconsin$양성여부)
+tr_data2 <- createDataPartition(y=wisconsin2$양성여부,p=0.7,list=FALSE)
+train_Bayes <- wisconsin2[tr_data2,]
+test_Bayes <- wisconsin2[-tr_data2,]
+
+# (3) 나이브 베이즈 모델 생성
+model_Bayes <- naiveBayes(양성여부~.,data=train_Bayes)
+model_Bayes
+
+# (4) 예측값 생성
+predicted <- predict(model_Bayes, test_Bayes, type = "class")
+table(predicted,test_Bayes$양성여부)
+
+positiveTest <- as.factor(test_Bayes$양성여부)
+confusionMatrix(predicted,positiveTest)
+
 
 
 
